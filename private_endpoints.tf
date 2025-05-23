eeae2140355651aa96e4d635015998eaeaef248b
Widianto <aku@widianto.org>
2025-05-23 16:14:32 +0700

    modified:   private_endpoints.tf

--- private_endpoints.tf
+++ private_endpoints.tf
@@ -3,20 +3,6 @@ resource "azurerm_private_dns_zone" "mysql" {
   resource_group_name = azurerm_resource_group.main.name
 }
 
-#resource "azurerm_private_endpoint" "mysql" {
-#  name                = "pe-mysql"
-#  location            = var.location
-#  resource_group_name = azurerm_resource_group.main.name
-#  subnet_id           = azurerm_subnet.subnet.id
-#
-#  private_service_connection {
-#    name                           = "mysql-privateservice"
-#    private_connection_resource_id = azurerm_mysql_flexible_server.mysql.id
-#    subresource_names              = ["mysqlServer"]
-#    is_manual_connection           = false
-#  }
-#}
-
 resource "azurerm_private_dns_zone_virtual_network_link" "mysql_link" {
   name                  = "mysql-dnslink"
   resource_group_name   = azurerm_resource_group.main.name
